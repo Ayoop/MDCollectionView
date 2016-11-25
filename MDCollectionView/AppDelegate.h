55e27dc88d5f4b5e6f03c34309dcbb7396a9d733
joey520 <caowenwen@caowenwendemacbook-pro.local>
2016-11-25 20:56:04 +0800

    Initial Commit

---
+++ MDCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MDCollectionView
+//
+//  Created by joey on 16/11/25.
+//  Copyright © 2016年 com.joey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MDCollectionView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MDCollectionView
+//
+//  Created by joey on 16/11/25.
+//  Copyright © 2016年 com.joey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
